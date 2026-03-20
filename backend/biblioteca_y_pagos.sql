0b0bccdece1444af6cee8a54f050cff4fd3d955e
GorchRosas <jorgealbertobarriosrosas2@gmail.com>
2026-03-20 12:23:18 -0600

    Add files via upload

---
+++ backend/biblioteca_y_pagos.sql
@@ -0,0 +1,47 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 20-03-2026 a las 19:22:31
+-- Versión del servidor: 10.4.32-MariaDB
+-- Versión de PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `biblioteca`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `biblioteca y pagos`
+--
+
+CREATE TABLE `biblioteca y pagos` (
+  `Num. de cuenta` int(9) NOT NULL,
+  `adeudo de biblioteca` tinyint(1) NOT NULL,
+  `Num. de referencia` int(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `biblioteca y pagos`
+--
+
+INSERT INTO `biblioteca y pagos` (`Num. de cuenta`, `adeudo de biblioteca`, `Num. de referencia`) VALUES
+(323213123, 1, 111),
+(52424155, 0, 35226267);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
